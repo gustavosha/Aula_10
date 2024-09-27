1dd9b02926c5923df12a221720128e6b8567a652
gustavosha <116170062+gustavosha@users.noreply.github.com>
2024-09-27 12:27:30 -0300

    Add files via upload

---
+++ Aula10_Projeto_Templeite_Strings/script.js
@@ -0,0 +1,8 @@
+let nome = 'Marcelo';
+let sobrenome = 'Rubens';
+let idade = 54;
+
+console.log ("Meu nome é",nome,sobrenome,"e minha idade é",idade,"anos");
+
+let mensagem = `Meu nome é ${nome} ${sobrenome} e minha idade ${idade} anos`;
+console.log(mensagem);
